4c7173c725f3adea767d0aaa1a304dca604617ac
Sameer Singh <sameer.ctinfotech@gmail.com>
2024-11-25 20:02:18 +0530

    .

--- lib/pages/mape/mapeController.dart
+++ lib/pages/mape/mapeController.dart
@@ -8,8 +8,7 @@ import '../../services/base_client01.dart';
 import 'mapeModel.dart';
 
 class MapeUserController extends GetxController implements GetxService {
-  RxBool isLoadig = false.obs;
-  List<Bussiness> userBusinessMap = [];
+
 
   @override
   void onReady() {
@@ -22,29 +21,7 @@ class MapeUserController extends GetxController implements GetxService {
   var userProfileImage;
 
   getAllUserMape(String search) async {
-    // isLoadig(true);
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    var userid = prefs.get("user_id");
-    var token = prefs.get('token');
-    isLoadig(true);
-    print("MapeApi search====>>>>>${search}");
-    final response = await BaseClient01().post(Appurls.mapUrl, {
-      "user_id": userid.toString(),
-      if (search != null) "search": search ?? "n",
-    });
-    print(response);
-    print("ultramodern1");
 
-    bool success = response["success"];
-
-    if (success) {
-      final data = MapeBusinessModel.fromJson(response);
-      userBusinessMap = data.bussiness;
-      update();
-    }
-    update();
-    // print("controller data ${controller}");
-    // cont(false);
     Map<String, dynamic> apiData = await ApiCall.instance
         .callApi(url: "https://forreal.net:4000/users/nearByBussiness", headers: await authHeader(), method: HttpMethod.POST, body: {
       "user_id": await getUserId(),
