72a0c9d3ba93b562f28bd1024d33abb54e7f6a53
Pramod Vishwakarma <pramod00108@gmail.com>
2024-11-29 15:19:06 +0530

    .

--- lib/buisness_screens/buisness_create_ads/edit_ads/edit_ads_controller.dart
+++ lib/buisness_screens/buisness_create_ads/edit_ads/edit_ads_controller.dart
@@ -50,10 +50,10 @@ class EditAdsController extends GetxController {
   ) async {
     print("tittle text");
     print(Title.value.text);
-    await ApiCall.instance.callApi(
+    Map<String, dynamic> apiData = await ApiCall.instance.callApi(
       url: "https://forreal.net:4000/update_advs",
       method: HttpMethod.POST,
-      body: {
+      body: dio.FormData.fromMap({
         "id": addId.toString(),
         "title": Title.text.isEmpty ? myAdv!.title : Title.text,
         "interest": Intrest.text.isEmpty ? myAdv!.interest : Intrest.text,
@@ -62,84 +62,28 @@ class EditAdsController extends GetxController {
         "address": Location.text.isEmpty ? myAdv!.address : Location.text,
         "range_km": Range.text.isEmpty ? myAdv!.rangeKm.toString() : Range.text,
         "link": Link.text.isEmpty ? myAdv!.link.toString() : Link.text,
-      },
+        if (addImage != null)
+          "file": (await dio.MultipartFile.fromFile(addImage!, filename: "${DateTime.now().toUtc().toIso8601String()}.jpg"))
+      }),
       headers: await authHeader(),
     );
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    var userId = prefs.get('user_id');
-    var token = prefs.get('token');
-    const apiUrl = 'https://forreal.net:4000/update_advs'; // Replace with your API endpoint
-    final headers = {
-      'Content-Type': 'multipart/form-data',
-      'Authorization': 'Bearer $token',
-    };
-    print("userid Cover$userId");
-    try {
-      final request = http.MultipartRequest('POST', Uri.parse(apiUrl));
-      request.headers.addAll(headers);
-      request.fields["id"] = addId.toString();
-      request.fields["title"] = Title.text.isEmpty ? myAdv!.title : Title.text;
-      request.fields["age"] = Age.text.isEmpty ? myAdv!.age.toString() : Age.text;
-      request.fields["interest"] = Intrest.text.isEmpty ? myAdv!.interest : Intrest.text;
-      request.fields["budget"] = budget.text.isEmpty ? myAdv!.budget.toString() : budget.text;
-      request.fields["campaign_duration"] = CampaignDuration.text.isEmpty ? myAdv!.campaignDuration : CampaignDuration.text;
-      request.fields["address"] = Location.text.isEmpty ? myAdv!.address : Location.text;
-      request.fields["range_km"] = Range.text.isEmpty ? myAdv!.rangeKm.toString() : Range.text;
-      request.fields["link"] = Link.text.isEmpty ? myAdv!.link.toString() : Link.text;
 
-      // Add the image file to the request
+    if (apiData["success"] == true) {
+      // Handle a successful response
 
-      if (addImage == null) {
-        var fileName = allAdssDealController.getAllAdsMdoels?.myAdvs[index].adImage;
-        request.fields['file'] = fileName.toString();
-        print("File======>");
-        print(
-          request.fields['file'],
-        );
-      } else {
-        request.files.add(
-          await http.MultipartFile.fromPath('file', addImage.toString()),
-        );
-        print("file data");
-        print(request.fields['file']);
-        // https://forreal.net:4000/deals/
-      }
-
-      // if (File(addImage.toString()) != null) {
-      //   request.files.add(http.MultipartFile(
-      //     'file', // This should match the field name expected by your API
-      //     File(addImage.toString()).readAsBytes().asStream(),
-      //     File(addImage.toString()).lengthSync(),
-      //     filename: File(addImage.toString()).path.split('/').last,
-      //   ));
-      // }
-
-      final response = await request.send();
-      if (response.statusCode == 200) {
-        // Handle a successful response
-        final responseData = await response.stream.toBytes();
-        final responseString = String.fromCharCodes(responseData);
-        print('Response: $responseString');
-        print('Response: $response');
-        Get.off(() => All_Ads());
-        allAdssDealController.All_AdsD();
-        Fluttertoast.showToast(
-          msg: "updated Successfully.",
-          toastLength: Toast.LENGTH_LONG,
-          gravity: ToastGravity.BOTTOM,
-          timeInSecForIosWeb: 1,
-          backgroundColor: Colors.white,
-          textColor: Colors.black,
-          fontSize: 16.0,
-        );
-        print(request.fields);
-      } else {
-        // Handle API error
-        print('API Request Failed: ${response.reasonPhrase}');
-      }
-    } catch (e) {
-      // Handle network or other errors
-      print('Error: $e');
+      Get.off(() => All_Ads());
+      allAdssDealController.All_AdsD();
+      Fluttertoast.showToast(
+        msg: "updated Successfully.",
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.white,
+        textColor: Colors.black,
+        fontSize: 16.0,
+      );
+    } else {
+      // Handle API error
     }
   }
 
