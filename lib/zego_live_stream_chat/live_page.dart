4af8a905705879237fc643df410b34b3984f6883
Pramod Vishwakarma <pramod00108@gmail.com>
2024-11-30 21:07:14 +0530

    Merge remote-tracking branch 'github/main'

--- lib/zego_live_stream_chat/live_page.dart
+++ lib/zego_live_stream_chat/live_page.dart
@@ -10,7 +10,7 @@ import 'constatent.dart';
 class LivePage extends StatefulWidget {
   final String liveID;
   final bool isHost;
-  final String userName;
+  final String userNmae;
   final String userId;
   final String? image;
 
@@ -19,37 +19,26 @@ class LivePage extends StatefulWidget {
     required this.liveID,
     this.isHost = false,
     required this.userId,
-    required this.userName,
-    this.image,
+    required this.userNmae,
+    this.image
   });
 
   @override
-  State<StatefulWidget> createState() => _LivePageState();
+  State<StatefulWidget> createState() => LivePageState();
 }
 
-class _LivePageState extends State<LivePage> {
-  @override
-  void initState() {
-    super.initState();
-    debugPrint('Profile Image: ${widget.image}');
-  }
-
+class LivePageState extends State<LivePage> {
   Future<void> removeAllLiveUsers() async {
     try {
-      final liveUsersSnapshot =
-      await FirebaseFirestore.instance.collection("Liveusers").get();
+      final liveUsersSnapshot = await FirebaseFirestore.instance.collection("Liveusers").get();
 
       for (var doc in liveUsersSnapshot.docs) {
         await doc.reference.delete();
       }
-      debugPrint("All live user data removed successfully.");
+      print("All live user data removed successfully.");
     } catch (e) {
-      debugPrint("Error removing live users: $e");
+      print("Error removing live users: $e");
     }
-  }
-
-  Future<void> _endLiveSession() async {
-    await removeAllLiveUsers();
   }
 
   @override
@@ -60,6 +49,10 @@ class _LivePageState extends State<LivePage> {
     super.dispose();
   }
 
+  Future<void> _endLiveSession() async {
+    await removeAllLiveUsers();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -68,42 +61,40 @@ class _LivePageState extends State<LivePage> {
           if (widget.isHost) {
             await _endLiveSession();
           }
+          return;
         },
         child: ZegoUIKitPrebuiltLiveStreaming(
           events: ZegoUIKitPrebuiltLiveStreamingEvents(
             onEnded: (event, defaultAction) {
-              if (!widget.isHost) {
+              if (widget.isHost == false) {
                 _showLiveEndedPopup();
               } else {
                 Get.back();
               }
+              print("line 65");
             },
           ),
           appID: 426171095,
-          appSign:
-          '02d978a9a7bda152641751dd6629f656e5c7412f238903b87b36155f0fd3474f', // Replace with your AppSign
-          userID: widget.userId,
-          userName: widget.userName,
+          appSign: '02d978a9a7bda152641751dd6629f656e5c7412f238903b87b36155f0fd3474f',
+          userID: localUserID,
+          userName: widget.userNmae,
           liveID: widget.liveID,
-          config: (widget.isHost
-              ? ZegoUIKitPrebuiltLiveStreamingConfig.host()
-              : ZegoUIKitPrebuiltLiveStreamingConfig.audience())
-            ..avatarBuilder = (context, size, user, extraInfo) {
-              if (widget.isHost || !widget.isHost) {
-                return null;
-              }
-              return null;
-            },
+          config: (widget.isHost ? ZegoUIKitPrebuiltLiveStreamingConfig.host() : ZegoUIKitPrebuiltLiveStreamingConfig.audience())
+
         ),
       ),
     );
   }
+}
 
-  void _showLiveEndedPopup() {
-    Get.dialog(
+
+void _showLiveEndedPopup() {
+  Get.dialog(
       barrierDismissible: false,
       WillPopScope(
-        onWillPop: () async => false,
+        onWillPop: () async {
+          return false;
+        },
         child: AlertDialog(
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(15.0),
@@ -120,20 +111,20 @@ class _LivePageState extends State<LivePage> {
               onPressed: () {
                 Get.back();
                 Get.back();
-                _endLiveSession();
               },
               child: const Text('Close'),
             ),
+            // ElevatedButton(
+            //   onPressed: () {
+            //    Get.back();
+            //    Get.back();
+            //     // Add any additional actions here
+            //   },
+            //   child: const Text('Go to Home'),
+            // ),
           ],
         ),
-      ),
-    );
-  }
+      ));
 }
-
-
-
-
-
 
 
