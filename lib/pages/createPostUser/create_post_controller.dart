176477be0a50dbb5b1ee139d290f2265e31b8b83
Pramod Vishwakarma <pramod00108@gmail.com>
2024-12-10 16:20:05 +0530

    .

--- lib/pages/createPostUser/create_post_controller.dart
+++ lib/pages/createPostUser/create_post_controller.dart
@@ -1,9 +1,14 @@
 import 'package:camera/camera.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:video_compress/video_compress.dart';
+
+import 'createUserPost.dart';
 
 class CreatePostController extends GetxController {
   final Rxn<XFile> selectedFile = Rxn<XFile>();
+  final Rxn<fileTypeName> selectedFileType = Rxn<fileTypeName>();
 
   @override
   void onInit() {
@@ -11,39 +16,64 @@ class CreatePostController extends GetxController {
   }
 
   void onTapGallery() {
-    ImagePicker picker = ImagePicker();
+    final ImagePicker picker = ImagePicker();
     picker
         .pickMedia(
       imageQuality: 50,
     )
         .then(
-      (value) {
+      (value) async {
         if (value != null) {
+          selectedFile.value = null;
+          selectedFileType.value = null;
           selectedFile.value = value;
+          selectedFileType.value = fileTypeCheckk(selectedFile.value!.path!);
+          if (selectedFileType.value == fileTypeName.Video) {
+            final directory = await getTemporaryDirectory();
+            MediaInfo? mediaInfo = await VideoCompress.compressVideo(
+              '${directory.path}/temp_${DateTime.now().millisecondsSinceEpoch}.mp4',
+              quality: VideoQuality.DefaultQuality,
+            );
+            selectedFile.value = XFile(mediaInfo!.path!);
+          }
         }
       },
     );
   }
 
   void onTapVideo() {
-    ImagePicker picker = ImagePicker();
+    final ImagePicker picker = ImagePicker();
     picker.pickVideo(source: ImageSource.camera).then(
-      (value) {
+      (value) async {
         if (value != null) {
+          selectedFile.value = null;
+          selectedFileType.value = null;
           selectedFile.value = value;
+          selectedFileType.value = fileTypeCheckk(selectedFile.value!.path!);
+          final directory = await getTemporaryDirectory();
+          MediaInfo? mediaInfo = await VideoCompress.compressVideo(
+            '${directory.path}/temp_${DateTime.now().millisecondsSinceEpoch}.mp4',
+            quality: VideoQuality.DefaultQuality,
+          );
+          selectedFile.value = XFile(mediaInfo!.path!);
         }
       },
     );
   }
 
   void onTapCamera() {
-    ImagePicker picker = ImagePicker();
+    final ImagePicker picker = ImagePicker();
     picker.pickVideo(source: ImageSource.gallery).then(
       (value) {
         if (value != null) {
+          selectedFile.value = null;
+          selectedFileType.value = null;
           selectedFile.value = value;
+          selectedFileType.value = fileTypeCheckk(selectedFile.value!.path!);
         }
       },
     );
   }
+
+
 }
